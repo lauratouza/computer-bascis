15a60958e3de8c0f9e22b3f1cf003079540766ef
lauratouza <ltouzdeo@myuax.com>
2024-10-08 17:33:52 +0200

    Add files via upload

---
+++ prueba celsius.py
@@ -0,0 +1,6 @@
+farenh= int (input("introducir la temperatura a convertir: "))
+def celsius(farenh):
+    resultado= ((farenh*9/5)+32)
+    return resultado
+print ("la temperatura es:", celsius (farenh))
+
---
+++ prueba kinetic energy.py
@@ -0,0 +1,10 @@
+# Get user input for mass and velocity of the object
+mass = float(input("Enter the mass (in kilograms): "))
+velocity = float(input("Enter the velocity (in m/s): "))
+kinetic_energy=(0.5*mass*velocity**2)
+
+print (" The Kinetic Energy is: ", kinetic_energy)
+
+  
+
+
